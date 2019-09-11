834f8d93b6230330fc1c1b2e49670c41f09b2031
Joao Lobo <joao.lobom@gmail.com>
2019-09-11 21:19:58 +0200

    going

--- app/controllers/api/v1/ports_controller.rb
+++ app/controllers/api/v1/ports_controller.rb
@@ -1,10 +1,24 @@
+
 module Api
-    module V1
-        class PortsController < ApplicationController
-            def index
-                @ports = Port.all
-                render json: {status: 'Sucess', data:@ports}
-            end
-    end
-end
-end
+  module V1
+    class PortsController < ApplicationController
+
+        def index
+        @ports = Port.all
+        render json: {status: 'Sucess' data:@ports}
+        end
+
+        def show
+          @ports = Port.find(params[:id])
+          render json: {status: 'Sucess' data:@ports}
+        end
+        
+        def create
+          @ports = Port.new(article_params) 
+        end
+
+        private
+
+        def article_params
+          params.permit(:title, :body)
+        end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
-Port.create({
-    nr: 4746,
-    name: "Aeropuerto General Tomas de Heres Ciudad Bolivar",
-    code: "CBL",
-    city: "17",
-    oceaninsightscode: "hey",
-    latitude: 8.12216091156,
-    longitude: -63.5369567871,
-    bigschedules: "ka",
-    createdat: 2017-12-31,
-    updatedat: 2017-12-31,
-    porttype: "airport",
-    hubport: false,
-    oceaninsights: "koko",
-})
+# frozen_string_literal: true
+Port.create(
+  nr: 4746,
+  name: 'Aeropuerto General Tomas de Heres Ciudad Bolivar',
+  code: 'CBL',
+  city: '17',
+  oceaninsightscode: 'hey',
+  latitude: 8.12216091156,
+  longitude: -63.5369567871,
+  bigschedules: 'ka',
+  createdat: 2017 - 12 - 31,
+  updatedat: 2017 - 12 - 31,
+  porttype: 'airport',
+  hubport: false,
+  oceaninsights: 'koko'
+)
