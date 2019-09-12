0591d7285156eb7ee965fedda21bd938942835f8
Joao Lobo <joao.lobom@gmail.com>
2019-09-12 12:31:14 +0200

    getting there

--- app/controllers/api/v1/ports_controller.rb
+++ app/controllers/api/v1/ports_controller.rb
@@ -1,24 +1,49 @@
+# frozen_string_literal: true
 
 module Api
   module V1
     class PortsController < ApplicationController
+      def index
+        ports = Port.all
+        render json: { status: 'SUCCESS', message: 'Loaded Ports', data: ports }, status: :ok
+      end
 
-        def index
-        @ports = Port.all
-        render json: {status: 'Sucess' data:@ports}
-        end
+      def show
+        ports = Port.find(params[:id])
+        render json: { status: 'SUCCESS', message: 'Loaded Portfound', data: ports }, status: :ok
+      end
 
-        def show
-          @ports = Port.find(params[:id])
-          render json: {status: 'Sucess' data:@ports}
-        end
-        
-        def create
-          @ports = Port.new(article_params) 
-        end
+      def create
+        ports = Port.new(ports_params)
+          port.save
+          render json: { status: 'SUCCESS', message: 'Saved article', data: article }, status: :ok
 
-        private
+      end
 
-        def article_params
-          params.permit(:title, :body)
-        end
+      def destroy
+        article = Article.find(params[:id])
+        article.destroy
+        render json: { status: 'SUCCESS', message: 'Deleted article', data: article }, status: :ok
+      end
+
+
+
+      private
+
+      def ports_params
+        params.permit(:name,
+                      :code,
+                      :city,
+                      :oceaninsightscode,
+                      :latitude,
+                      :longitude,
+                      :bigschedules,
+                      :createdat,
+                      :updatedat,
+                      :porttype,
+                      :hubport,
+                      :oceaninsights)
+      end
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,19 @@ Port.create(
   hubport: false,
   oceaninsights: 'koko'
 )
+Port.create(
+  nr: 11,
+  name: 'port2',
+  code: 'CBL',
+  city: '17',
+  oceaninsightscode: 'hey',
+  latitude: 8.12216091156,
+  longitude: -63.5369567871,
+  bigschedules: 'ka',
+  createdat: 2017 - 12 - 31,
+  updatedat: 2017 - 12 - 31,
+  porttype: 'airport',
+  hubport: false,
+  oceaninsights: 'koko'
+)
+
