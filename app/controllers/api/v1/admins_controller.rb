3f24772ae42a5132a12785f6d99eec557d48f81a
Joao Lobo <joao.lobom@gmail.com>
2019-09-15 20:52:36 +0200

    admin passwod control set up

--- app/controllers/api/v1/admins_controller.rb
+++ app/controllers/api/v1/admins_controller.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-
 module Api
-    module V1
-      class AdminsController < ApplicationController
+  module V1
+    class AdminsController < ApplicationController
       def index
-        admins = Admin.all
-        render json: { status: 'SUCCESS', message: 'Your result', data: admins }, status: :ok
+        if admins = Admin.where(username: params[:username].to_s, password: params[:password].to_s)
+          render json: { status: 'SUCCESS', message: 'Your token, if nothing displayed, your password or username might be incorrect', data: admins }, status: :ok
       end
 
-      def create
-        admin = Admin.create(username: params[:username], password: params[:password])
-        render json: { status: 'SUCCESS', message: 'Your result', data: admin }, status: :ok
-      end
+        def create
+          admin = Admin.create(username: params[:username], password: params[:password])
+          render json: { status: 'SUCCESS', message: 'Your result', data: admin }, status: :ok
+        end
   end
  end
+end
 end
