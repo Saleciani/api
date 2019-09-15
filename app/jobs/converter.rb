ac61376de57b059e0a7396a5d169021aaa499362
Joao Lobo <joao.lobom@gmail.com>
2019-09-15 14:04:18 +0200

    heroku fixed

--- app/jobs/converter.rb
+++ app/jobs/converter.rb
@@ -1,6 +1,6 @@
 require 'csv'
 require 'json'
 # frozen_string_literal: true
-data = File.open("/home/joao/Desktop/samples/airports2.csv").read
+#data = File.open("/home/joao/Desktop/samples/airports2.csv").read
 
-puts parsed = CSV.parse(data).to_json
+#puts parsed = CSV.parse(data).to_json
